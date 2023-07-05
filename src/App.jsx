a44085b08a5eb6463616a100a3a60c8253c19a55
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-07-05 11:30:27 -0700

    Added items to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,23 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Dashboard, { dashboardLoader } from './pages/Dashboard';
 import Error from './pages/Error';
 
+// Layouts
+import Main, { mainLoader } from './layouts/Main';
+
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Dashboard />,
-    loader: dashboardLoader,
+    element: <Main />,
+    loader: mainLoader,
     errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        errorElement: <Error />,
+      },
+    ],
   },
 ]);
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,33 @@
+// rrd imports
+import { Form, NavLink } from 'react-router-dom';
+
+// assets
+import logomark from '../assets/logomark.svg';
+
+const Nav = ({ userName }) => {
+  return (
+    <nav>
+      <NavLink to='/' aria-label='Go to home'>
+        <img src={logomark} alt='' height={30} />
+        <span>HomeBudget</span>
+      </NavLink>
+      {userName && (
+        <Form
+          method='post'
+          action='/logout'
+          onSubmit={(event) => {
+            if (!confirm('Delet user and all data?')) {
+              event.preventDefault();
+            }
+          }}
+        >
+          <button type='submit' className='btn btn--warning'>
+            <span>Delete User</span>
+          </button>
+        </Form>
+      )}
+    </nav>
+  );
+};
+
+export default Nav;
