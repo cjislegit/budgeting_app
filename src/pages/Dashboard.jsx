f178a13bce9f793b9835860544e193b0dacd2b3d
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-07-10 19:51:50 -0700

    Created the form for the new budget

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,11 +9,13 @@ import { fetchData } from '../helpers';
 
 // Components
 import Intro from '../components/Intro';
+import AddBudgetForm from '../components/AddBudgetForm';
 
 // loader
 export function dashboardLoader() {
   const userName = fetchData('userName');
-  return { userName };
+  const budgets = fetchData('budgets');
+  return { userName, budgets };
 }
 
 // action
@@ -29,8 +31,28 @@ export async function dashboardAction({ request }) {
 }
 
 const Dashboard = () => {
-  const { userName } = useLoaderData();
-  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
+  const { userName, budgets } = useLoaderData();
+  return (
+    <>
+      {userName ? (
+        <div className='dashboard'>
+          <h1>
+            Welcome back, <span className='accent'>{userName}</span>
+          </h1>
+          <div className='grid-sm'>
+            {/* {budgets ? () : ();} */}
+            <div className='grid-lg'>
+              <div className='flex-lg'>
+                <AddBudgetForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Intro />
+      )}
+    </>
+  );
 };
 
 export default Dashboard;
