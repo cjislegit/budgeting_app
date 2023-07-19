9122d83581a89923bce2bb6bee0f6dffffc710c9
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-07-19 08:07:50 -0700

    Added firebase

---
+++ src/firebaseSetup/firebase.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyCaU6avGt6D6A0RwBehtQlOVAH31XKHNT4',
+  authDomain: 'homebudget-6ace5.firebaseapp.com',
+  projectId: 'homebudget-6ace5',
+  storageBucket: 'homebudget-6ace5.appspot.com',
+  messagingSenderId: '631929967283',
+  appId: '1:631929967283:web:70af8df43be845289e16c6',
+  measurementId: 'G-PY5SMWLNFX',
+};
