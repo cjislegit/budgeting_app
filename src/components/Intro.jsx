c0bf68140ab9036db1f848ee91c341bf4472802c
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-07-19 11:26:25 -0700

    Changed login to include email and password instead of name. User login is now pushed to firebase

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,3 +1,7 @@
+// react
+import { useState } from 'react';
+
+//rrd
 import { Form } from 'react-router-dom';
 
 // library
@@ -6,7 +10,23 @@ import { UserPlusIcon } from '@heroicons/react/24/solid';
 // assets
 import illustration from '../assets/illustration.jpg';
 
+//firebase
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
 const Intro = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const auth = getAuth();
+
+  async function handleSignUp() {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   return (
     <div className='intro'>
       <div>
@@ -22,12 +42,30 @@ const Intro = () => {
             type='text'
             name='userName'
             required
-            placeholder='What is your name?'
-            aria-label='Your Name'
-            autoComplete='given-name'
+            placeholder='What is your Email?'
+            aria-label='Your Email'
+            autoComplete='email'
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type='password'
+            name='password'
+            required
+            placeholder='Create Password'
+            aria-label='Create Password'
+            autoComplete='new-password'
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
           <input type='hidden' name='_action' value='newUser' />
-          <button type='submit' className='btn btn--dark'>
+          <button
+            type='submit'
+            className='btn btn--dark'
+            onClick={() => handleSignUp()}
+          >
             <span>Create Account</span>
             <UserPlusIcon width={20} />
           </button>
